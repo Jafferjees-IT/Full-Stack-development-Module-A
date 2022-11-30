2bc0722ca6ebde22fad797d6015371c04d830ca4
Fakhruddin Kaukawala <fakhruddin@jafferjees.com>
2022-11-30 15:24:30 +0500

    Update Email Validiation
    Update Email validation

--- project 1/script.js
+++ project 1/script.js
@@ -26,12 +26,21 @@ function showSuccess(input){
 
 }
 
+function isValidEmail(email){
+    return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+}
+
 // Event Listener
 // Create event listener for Submit Button
 form.addEventListener('submit', function(e) {
     // stop reload on submit
     e.preventDefault();
 
+    // Check required field not empty
     // check if name is empty
     if(username.value === '') {
         showError(username, 'Username is Requird');
@@ -42,6 +51,8 @@ form.addEventListener('submit', function(e) {
    
     if(email.value === '') {
         showError(email, 'Email is Requird');
+    } else if(!isValidEmail(email.value)) {
+        showError(email, 'Invalid Email Address');
     }
     else {
         showSuccess(email);
