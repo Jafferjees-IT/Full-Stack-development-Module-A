f80afeac488f816bc297783c7cb51396b48b6f00
Fakhruddin Kaukawala <fakhruddin@jafferjees.com>
2022-11-30 11:35:22 +0500

    Update script.js
    Update showerror function

--- project 1/script.js
+++ project 1/script.js
@@ -1,4 +1,4 @@
-// Retriveing HTML elements from the DOM
+// Retriveing HTML elements from the DOM (Document Object Model)
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
@@ -6,10 +6,11 @@ const password = document.getElementById('password');
 const password2 =document.getElementById('password2');
 
 // functions start from 1:00:00
-function showError(input, message) {
-    const formcontrol = input.parentElement;
-    formcontrol.classname;
-    
+function showError(input, message){
+    const formControl = input.parentElement;
+    formControl.className = 'from-Control error'
+    const small = formControl.querySelector('small');
+    small.innerText = message;
 }
 
 // Event Listener
@@ -21,7 +22,7 @@ form.addEventListener('submit', function(e) {
     // check if name is empty
     if(username.value === '')
     {
-        showError();
+        showError(username, 'Username is Requird');
     }
    
 });
