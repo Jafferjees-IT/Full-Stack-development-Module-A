b6372e3dba9605ef7e59b64b250a54d031731a68
Fakhruddin Kaukawala <fakhruddin@jafferjees.com>
2022-11-30 12:18:11 +0500

    Updated
    Updated

--- project 1/script.js
+++ project 1/script.js
@@ -3,14 +3,27 @@ const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const password2 =document.getElementById('password2');
+const password2 = document.getElementById('password2');
 
 // functions start from 1:00:00
 function showError(input, message){
+    // Get the parent element of the input field (.from-control)
     const formControl = input.parentElement;
+    // override the class  - add error
     formControl.className = 'from-Control error'
+    // Get the small element for the error message
     const small = formControl.querySelector('small');
+    // override the text for small message
     small.innerText = message;
+}
+
+// Function to update class for success
+function showSuccess(input){
+    // Get the parent element of the input field (.from-control)
+    const formControl = input.parentElement;
+    // override the class  - add error
+    formControl.className = 'from-Control success'
+
 }
 
 // Event Listener
@@ -23,6 +36,10 @@ form.addEventListener('submit', function(e) {
     if(username.value === '')
     {
         showError(username, 'Username is Requird');
+    }
+    else
+    {
+        showSuccess(username);
     }
    
 });
