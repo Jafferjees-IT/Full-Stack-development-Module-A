481351218bb11f5ba4776d38cd3dce575d795793
Fakhruddin Kaukawala <fakhruddin@jafferjees.com>
2022-12-06 16:23:00 +0500

    Changes on 061222

--- project 2/script.js
+++ project 2/script.js
@@ -1,18 +1,46 @@
 // Get DOM Element
 const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.row.seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
-const connt = document.getElementById('total');
+const total = document.getElementById('total');
 const movieSelect = document.getElementById('Movie');
-const ticketPrice = +movieSelect.value; // if we plece + sign its convert into number
+let ticketPrice = +movieSelect.value; // if we plece + sign its convert into number
 
+function updateSelectedCount(){
+    const selectedSeats = document.querySelectorAll('.row .seat.selected'); // Get Selected list
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));  //  copy selected Seats make Comma Saprated list and get seat number
+    console.log(seatsIndex);
+    const SelectedSeatsCount = selectedSeats.length; // Get Numbers of selected seat
+    count.innerText = SelectedSeatsCount;
+    total.innerText = SelectedSeatsCount *  ticketPrice;
+    localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex)); //Json stringfy makes array to string
+}
+
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMoiveIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+
+}
+
+// Event Listener for container to check and click on seat
 container.addEventListener('click', (e) => { // we can use function(e)  instant of (e)=> {}
     if(e.target.classList.contains('seat') && 
         !e.target.classList.contains('occupied')
     ){
-        e.target.classList.toggle('selected')
+        e.target.classList.toggle('selected');
+        
+        updateSelectedCount();
 
     }
-})  
+}) 
+
+//Event listern for Movie pickup
+movieSelect.addEventListener('change',(e) => {
+    ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
+    updateSelectedCount();
+})
+
+
 
 
